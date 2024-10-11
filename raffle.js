2666c46e5db29d2039604c0061060e451245f23b
peacefrom-earth <thewish.club@protonmail.com>
2024-10-11 14:54:38 -0400

    Update raffle.js

--- raffle.js
+++ raffle.js
@@ -1,5 +1,5 @@
 // Base URL for the API
-const BASE_URL = 'https://peacefrom.earth';
+const BASE_URL = 'https://peacefrom.earth/wish_append.html';
 
 // Wait for the DOM to be fully loaded before running the script
 document.addEventListener('DOMContentLoaded', function() {
